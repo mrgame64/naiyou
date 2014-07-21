645a2760204458636db09a9a76273a2d05bac881
James Game <james.game.64@gmail.com>
2014-07-21 22:00:54 +0000

    First version of content.js

---
+++ routes/content.js
@@ -0,0 +1,10 @@
+GLOBAL.PostSchema = new Schema({
+    id:           { type: Number, required: true, unique: true },
+    page:         { type: Boolean, required: true},
+    tags:         [{ type: String, trim: true }],
+    categories:   [{ type: String, trim: true }],
+    title:        { type: String, required: true, trim: true },
+    file:         { type: String, required: true, trim: true },
+    creationDate: { type: Number, required: true },
+    editDate:     { type: Number, required: true }
+});
