a16078c6caae87ca78061478823efd40a30c31e7
James Game <james.game.64@gmail.com>
2014-07-21 22:30:30 +0200

    Update the router and API

--- routes/router.js
+++ routes/router.js
@@ -1,19 +1,19 @@
-var pages = require('./pages.js');
+var auth = require('./auth.js');
+var content = require('./content.js');
+var users = require('./users.js');
+var dashboard = require('./dashboard.js');
 
 
 app.use(express.static('static'));
 
 //API
-/*
-app.all('/api/access', access.api);
-app.all('/api/posts', content.apiPosts);
-app.all('/api/comments', content.apiComments);
-app.all('/api/pages' pages.api);
+app.all('/api/auth', auth.api);
+app.all('/api/users', users.api);
+app.all('/api/content', content.api);
 app.all('/api/dashboard', dashboard.api);
-*/
 
 //Pages
-app.get('/', pages.main);
-//app.get('/page', pages.page);
-//app.get('/post', pages.post);
-//app.get('/dashboard', dashboard.page);
+app.get('/', content.main);
+app.get('/page', content.page);
+app.get('/post', content.post);
+app.get('/dashboard', dashboard.page);
