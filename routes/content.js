e5847736f1e4e38b5b029c381e857794672a5a72
James Game <james.game.64@gmail.com>
2014-11-18 20:38:45 +0100

    Updated Entry model

--- routes/content.js
+++ routes/content.js
@@ -1,16 +1,16 @@
-GLOBAL.PostSchema = new Schema({
-    id:           { type: Number, required: true, unique: true },
-    page:         { type: Boolean, required: true},
-    tags:         [{ type: String, trim: true }],
-    categories:   [{ type: String, trim: true }],
-    title:        { type: String, required: true, trim: true },
-    file:         { type: String, required: true, trim: true },
-    creationDate: { type: Number, required: true },
-    editDate:     { type: Number, required: true }
+GLOBAL.EntrySchema = new Schema({
+    title:          { type: String, required: true, trim: true },
+    date:           { type: Number, required: true },
+    author:         { type: ObjectId, required: true},
+    editDate:       { type: Number },
+    editor:         { type: ObjectId }
+    post:           {
+        isPost: { type: Boolean, required: true},
+        id: { type: Number, required: true, unique: true }
+    },
+    content:        { type: String, required: true, trim: true },
+    tags:           [{ type: String, trim: true }],
+    comments:       [{ type: ObjectId }]
 });
 
-GLOBAL.Post = mongoose.model('Post', PostSchema);
-
-exports.api = function(){
-    
-};
+GLOBAL.Entry = mongoose.model('Entry', EntrySchema);
